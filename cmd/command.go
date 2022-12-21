43328381a4f6633f1ac76aa92a765d55842e01f5
K. Pastor-Gerte <konstantin@lastbackend.com>
2022-12-22 00:52:49 +0300

    modify redis plugin

--- cmd/command.go
+++ cmd/command.go
@@ -25,7 +25,7 @@ type Commands []*Command
 
 type Command struct {
 	// Use is the one-line usage message.
-	// Recommended syntax is as follow:
+	// Recommended syntax is as follows:
 	//   [ ] identifies an optional argument. Arguments that are not enclosed in brackets are required.
 	//   ... indicates that you can specify multiple values for the previous argument.
 	//   |   indicates mutually exclusive information. You can use the argument to the left of the separator or the
